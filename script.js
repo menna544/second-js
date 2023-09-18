bf5113e8beb4bac2d437757b226c377be87624ea
menna544 <116039213+menna544@users.noreply.github.com>
2023-09-18 22:54:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstname = "menna"
+const lastname = "maged"
+const age = 21
+const allparagraphs=document.getElementsByTagName("p")
+allparagraphs[0].textContent="firstname"
+allparagraphs[1].textContent="lastname"
+allparagraphs[2].textContent="Age"
+const element =document.querySelector("div")
+element.classList.add("style")
+console.error("Done")
